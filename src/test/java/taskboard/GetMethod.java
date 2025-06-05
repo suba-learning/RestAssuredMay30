834304b9bd0b216917eb25df7d562b6bf7e0a013
Suba Narayanan <ksuba.learning@gmail.com>
2025-06-04 19:37:01 -0700

    First Playright file

---
+++ src/test/java/taskboard/GetMethod.java
@@ -0,0 +1,40 @@
+package taskboard;
+
+import io.restassured.response.Response;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class GetMethod {
+
+    @BeforeEach
+    public void setup() {
+        // Classic style
+        baseURI = "https://taskboard.portnov.com/";
+    }
+
+    @Test
+    public void getTask() {
+        // Classic style
+        baseURI="https://taskboard.portnov.com/";
+        Response response = get("api/Task");
+        System.out.println(response.statusCode());
+        int status = response.statusCode();
+        assertEquals(200, status);
+    }
+
+    @Test
+    public void getTaskBdd() {
+        // Classic style
+        baseURI="https://taskboard.portnov.com/";
+        Response response = get("api/Task");
+        given()
+                .get("api/Task")
+                .then()
+                .statusCode(200).log();
+    }
+
+
+}
---
+++ src/test/java/taskboard/PostMethod.java
@@ -0,0 +1,48 @@
+package taskboard;
+
+import io.restassured.path.json.JsonPath;
+import org.json.JSONObject;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PostMethod {
+
+    @BeforeEach
+    public void setup() {
+        baseURI = "https://taskboard.portnov.com";
+    }
+
+    @Test
+    public void postTask() {
+        JSONObject jsonObject = new JSONObject();
+
+        jsonObject.put("id", 0);
+        jsonObject.put("taskName", "Suba Task IJ");
+        jsonObject.put("description", "Test Task");
+        jsonObject.put("dueDate", "2025-06-03T02:35:10.378Z");
+        jsonObject.put("priority", 1);
+        jsonObject.put("status","TODO");
+        jsonObject.put("author", "Suba");
+        System.out.println(jsonObject);
+        String stringBody = jsonObject.toString();
+
+        Response response = given()
+                .contentType(ContentType.JSON)
+                .body(stringBody)
+                .when()
+                .post("/api/Task/");
+
+        JsonPath jp = response.jsonPath();
+
+        response.prettyPrint();
+        assertEquals(201, response.statusCode());
+        assertEquals("Suba Task IJ",jp.getString("taskName"));
+
+    }
+
+}
